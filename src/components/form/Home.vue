c09e162c5fcf0075d6f1380c822424172edb2963
riyan-eng <fbriyant11@gmail.com>
2023-05-25 09:34:04 +0700

    update: form

---
+++ src/components/form/Home.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="flex grid p-2">
+        <form @submit.prevent="handleSubmit">
+            <label for="" class="my-2">Nama</label>
+            <InputText v-model="name" type="text" class="w-full md:w-14rem my-2" placeholder="Masukkan nama"/>
+            <label for="" class="my-2">Umur</label>
+            <InputNumber v-model="age" inputId="integeronly" class="w-full md:w-14rem my-2" placeholder="Masukkan umur"/>
+            <label for="" class="my-2">Gender</label>
+            <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Pilih jenis kelamin" class="w-full md:w-14rem my-2" />
+            
+            <Button class="my-2" label="Submit" type="submit"/>
+
+        </form>
+
+    </div>
+</template>
+
+<script setup>
+import InputText from 'primevue/inputtext';
+import InputNumber from 'primevue/inputnumber';
+import Dropdown from 'primevue/dropdown';
+import Button from 'primevue/button';
+import { ref } from "vue";
+
+const name = ref();
+const age = ref();
+const selectedCity = ref();
+const cities = ref([
+    { name: 'Laki-laki', code: 'L' },
+    { name: 'Perempuan', code: 'P' },
+]);
+
+function handleSubmit(params) {
+    console.log("halo")
+    console.log(name.value)
+    console.log(age.value)
+    console.log(selectedCity.value.code)
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,8 @@
 <template>
     <h1 class="bg-blue-600">Halaman home</h1>
-    <div class="card flex flex-col justify-content-center">
-        <InputText type="text" />
-        <Button label="Bayu saptaji"/>
-    </div>
+    <Form/>
 </template>
 
 <script setup>
-import Button from "primevue/button"
-import InputText from "primevue/inputtext";
+import Form from "@/components/form/Home.vue";
 </script>
