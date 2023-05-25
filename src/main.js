ecfb92f87ed3b05da272036fd43445c9d502a5d6
riyan-eng <fbriyant11@gmail.com>
2023-05-25 08:26:23 +0700

    update: kas

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
-import './assets/main.css'
+// import './assets/main.css'
+import 'primeflex/primeflex.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+
 
 import App from './App.vue'
 import router from './router'
+
+//theme
+import "primevue/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primevue/resources/primevue.min.css";
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,12 @@
 <template>
-    <h1>Halaman home</h1>
+    <h1 class="bg-blue-600">Halaman home</h1>
+    <div class="card flex flex-col justify-content-center">
+        <InputText type="text" />
+        <Button label="Bayu saptaji"/>
+    </div>
 </template>
+
+<script setup>
+import Button from "primevue/button"
+import InputText from "primevue/inputtext";
+</script>
