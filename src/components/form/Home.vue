85ce2edab6fff3d0fd3862fdeaff33a0eea912fa
riyan-eng <fbriyant11@gmail.com>
2023-05-29 10:12:19 +0700

    get post

--- src/components/form/Home.vue
+++ src/components/form/Home.vue
@@ -2,13 +2,15 @@
     <div class="flex grid p-2">
         <form @submit.prevent="handleSubmit">
             <label for="" class="my-2">Nama</label>
-            <InputText v-model="name" type="text" class="w-full md:w-14rem my-2" placeholder="Masukkan nama"/>
+            <InputText v-model="reqBody.name" type="text" class="w-full md:w-14rem my-2" placeholder="Masukkan nama" />
             <label for="" class="my-2">Umur</label>
-            <InputNumber v-model="age" inputId="integeronly" class="w-full md:w-14rem my-2" placeholder="Masukkan umur"/>
+            <InputNumber v-model="reqBody.age" inputId="integeronly" class="w-full md:w-14rem my-2"
+                placeholder="Masukkan umur" />
             <label for="" class="my-2">Gender</label>
-            <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Pilih jenis kelamin" class="w-full md:w-14rem my-2" />
-            
-            <Button class="my-2" label="Submit" type="submit"/>
+            <Dropdown v-model="reqBody.gender" :options="genders" optionLabel="name" placeholder="Pilih jenis kelamin"
+                class="w-full md:w-14rem my-2" />
+
+            <Button class="my-2" label="Submit" type="submit" />
 
         </form>
 
@@ -21,19 +23,43 @@ import InputNumber from 'primevue/inputnumber';
 import Dropdown from 'primevue/dropdown';
 import Button from 'primevue/button';
 import { ref } from "vue";
+import axios from 'axios'
 
-const name = ref();
-const age = ref();
-const selectedCity = ref();
-const cities = ref([
+const reqBody = ref({
+    name: "",
+    age: 0,
+    gender: null,
+});
+
+const genders = ref([
     { name: 'Laki-laki', code: 'L' },
     { name: 'Perempuan', code: 'P' },
 ]);
 
-function handleSubmit(params) {
-    console.log("halo")
-    console.log(name.value)
-    console.log(age.value)
-    console.log(selectedCity.value.code)
+async function handleSubmit() {
+    try {
+        // console.log("halo")
+        // console.log(reqBody.value.name)
+        // console.log(reqBody.value.age)
+        // console.log(reqBody.value.gender)
+        console.log(reqBody);
+        const url = 'http://localhost:3000/users'
+        // const fetcher = await fetch(url, {
+        //     method: 'post',
+        //     body: {
+        //         "id": 95,
+        //         "name": "banynay",
+        //         "age": 3,
+        //         "gender": null
+        //     }
+            
+            
+        // })
+        const fetcher = await axios.post(url, reqBody.value)
+        // const response = await fetcher.json()
+        console.log(fetcher)
+    } catch (error) {
+        console.log(error)
+    }
 }
 </script>
--- src/views/Bantuan.vue
+++ src/views/Bantuan.vue
@@ -1,3 +1,31 @@
 <template>
     <h1>Halaman bantuan</h1>
+    <table>
+        <thead>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>Umur</th>
+            <th>Kode Gender</th>
+        </thead>
+        <tbody>
+            <tr v-for="(item, index) in users" :key="index">
+                <td>{{ item.id }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.age }}</td>
+                <td>{{ item.gender }}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
+
+<script setup>
+import {ref} from 'vue'
+const users = ref(null);
+const url = 'http://localhost:3000/users'
+fetch(url).then(response => response.json()).then(
+    function(data) {
+        console.log(data);
+        users.value = data
+    }
+)
+</script>
